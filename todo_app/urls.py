6e3eda3ecf89ee9046385a47baf0685d5cfefd08
PythonAvodhaTamil <classroom2@avodha.co.in>
2022-05-26 14:58:12 +0530

    deplyment

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from todo_app import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.add_task,name='add_task'),
@@ -8,6 +9,8 @@ urlpatterns = [
     path('delete/<int:id>',views.delete,name='delete')
     
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
